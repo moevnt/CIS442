80ef42b59083b0c9e852f6d2581ea273ad5124bc
Evan Monteith <evan.monteith@gmail.com>
2020-02-11 21:03:34 -0500

    update

--- src/Classwork/BinaryTree.java
+++ src/Classwork/BinaryTree.java
@@ -3,42 +3,85 @@ package Classwork;
 //import sun.invoke.empty.Empty;
 
 public class BinaryTree <AnyType>{
-    private BinaryNode<AnyType> root;
+	private static BinaryNode root;
 
-    public BinaryTree(){
-        root = null;
-    }
+	public BinaryTree(){
+		root = null;
+	}
 
-    public BinaryTree(AnyType root){
-        this.root = new BinaryNode<AnyType>(root, null, null);
-    }
+	public BinaryTree(AnyType root){
+		this.root = new BinaryNode<AnyType>(root, null, null);
+	}
 
-    public BinaryNode<AnyType> getRoot() {
-        return root;
-    }
+	public BinaryNode<AnyType> getRoot() {
+		return root;
+	}
 
-    public void setRoot(BinaryNode<AnyType> root) {
-        this.root = root;
-    }
+	public void setRoot(BinaryNode<AnyType> root) {
+		this.root = root;
+	}
 
-    public void makeEmpty(){
-        this.root = null;
-    }
+	public void makeEmpty(){
+		this.root = null;
+	}
 
-    public boolean isEmpty(){
-        return root == null;
-    }
+	public boolean isEmpty(){
+		return root == null;
+	}
 
-    public void match(AnyType x, BinaryTree<AnyType> t1, BinaryTree<AnyType> t2){
-        root = new BinaryNode(x,t1.root,t2.root);
+	public void match(AnyType x, BinaryTree<AnyType> t1, BinaryTree<AnyType> t2){
+		root = new BinaryNode(x,t1.root,t2.root);
 
-        if(this != t1)
-            t1 = null;
-        if(this != t2)
-            t2 = null;
-    }
+		if(this != t1)
+			t1 = null;
+		if(this != t2)
+			t2 = null;
+	}
 
-    public int size(){
-        return BinaryNode.getSize(root);
-    }
+	private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> t) {
+		if (t == null)
+			return new BinaryNode<>(x, null, null);
+
+		t.left = insert(x, t.left);
+		return t;
+	}
+
+	public int size(){
+		return BinaryNode.getSize(root);
+	}
+
+	public int evenItems(BinaryNode t){
+		int count = 0;
+		for (int i=0;i<size();i++){
+			int element =(int) t.element;
+			if(element%2 == 0)
+				count++;
+			t=t.left;
+		}
+		return count;
+	}
+
+	public int sum(BinaryNode t){
+		int sum=0;
+		for (int i=0;i<size();i++){
+			sum += (int) t.element;
+			t = t.left;
+		}
+		return sum;
+	}
+
+
+	public static void main(String[] args){
+		BinaryTree tree = new BinaryTree<>();
+
+		root = new BinaryNode(21);
+
+		for (int i=0;i<10;i++){
+			int insert = (int) (Math.random()*100);
+			tree.insert(insert,root);
+		}
+		System.out.println(root.toString());
+		System.out.println("Sum: "+tree.sum(root));
+		System.out.println("number of even: "+tree.evenItems(root));
+	}
 }
