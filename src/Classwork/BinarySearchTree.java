ea67a011d2344beaa74704946d608288ca735c82
Evan Monteith <evan.monteith@gmail.com>
2020-02-11 20:21:45 -0500

    BST final

--- src/Classwork/BinarySearchTree.java
+++ src/Classwork/BinarySearchTree.java
@@ -1,30 +1,29 @@
-/*
-create
-Contains, Delete, FindMin, Find Max, insert
- */
-
-
 package Classwork;
 
+//HR: Evan Monteith, Textbook, In class notes
 import java.util.Comparator;
 
 public class BinarySearchTree<AnyType extends Comparable<AnyType>> {
 
-
-
 	private static BinaryNode root;
 	private Comparator<? super AnyType> cmp;
+	private AnyType element;
+	private BinaryNode<AnyType> left;
+	private BinaryNode<AnyType> right;
+	private int count;
+	private int leafCount;
+	private int fullNodeCount;
 
-	public BinarySearchTree(){
+	public BinarySearchTree() {
 		this(null);
-	}
-
-	public BinaryNode getRoot(){
-		return root;
 	}
 
 	public static void setRoot(BinaryNode root) {
 		BinarySearchTree.root = root;
+	}
+
+	public static BinaryNode getRoot() {
+		return root;
 	}
 
 	public BinarySearchTree(Comparator<? super AnyType> c) {
@@ -32,94 +31,96 @@ public class BinarySearchTree<AnyType extends Comparable<AnyType>> {
 		cmp = c;
 	}
 
-	public int myCompare(AnyType lhs, AnyType rhs){
-		if(cmp != null)
-			return cmp.compare(lhs,rhs);
+	private int myCompare(AnyType lhs, AnyType rhs) {
+		if (cmp != null)
+			return cmp.compare(lhs, rhs);
 		else
-			return ((Comparable)lhs).compareTo(rhs);
+			return ((Comparable) lhs).compareTo(rhs);
 	}
 
-	private boolean contains(AnyType x, BinaryNode<AnyType> t){
-		if(t == null)
+	private boolean contains(AnyType x, BinaryNode<AnyType> t) {
+		if (t == null)
 			return false;
 
-		int compareResult = myCompare( x, t.element );
+		int compareResult = myCompare(x, t.element);
 
-		if( compareResult < 0 )
-			return contains( x, t.left );
-		else if( compareResult > 0 )
-			return contains( x, t.right );
+		if (compareResult < 0)
+			return contains(x, t.left);
+		else if (compareResult > 0)
+			return contains(x, t.right);
 		else
-			return true; // Match
+			return true;
 	}
 
-	private BinaryNode<AnyType> findMin(BinaryNode<AnyType> t){
-		if(t == null)
+	private BinaryNode<AnyType> findMin(BinaryNode<AnyType> t) {
+		if (t == null)
 			return null;
 		else if (t.left == null)
 			return t;
 		return findMin(t.left);
 	}
 
-	private BinaryNode<AnyType> findMax(BinaryNode<AnyType> t){
-		if(t != null)
-			while(t.right != null)
+	private BinaryNode<AnyType> findMax(BinaryNode<AnyType> t) {
+		if (t != null)
+			while (t.right != null)
 				t = t.right;
-
 		return t;
 	}
 
-	private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> t){
-		if(t == null)
-			return new BinaryNode<>(x, null,null);
-		int compareResult = x.compareTo( t.element );
+	private BinaryNode<AnyType> insert(AnyType x, BinaryNode<AnyType> t) {
 
-		if( compareResult < 0 )
-			t.left = insert( x, t.left );
-		else if( compareResult > 0 )
-			t.right = insert( x, t.right );
+		if (t == null)
+			return new BinaryNode<>(x, null, null);
+
+		int compareResult = x.compareTo(t.element);
+
+		if (compareResult < 0)
+			t.left = insert(x, t.left);
+		else if (compareResult > 0)
+			t.right = insert(x, t.right);
 		else
-			; // Duplicate; do nothing
+			;
+
 		return t;
 	}
 
-	private BinaryNode<AnyType> remove( AnyType x, BinaryNode<AnyType> t ) {
-		if( t == null )
-			return t; // Item not found; do nothing
+	private BinaryNode<AnyType> remove(AnyType x, BinaryNode<AnyType> t) {
 
-		int compareResult = x.compareTo( t.element );
+		if (t == null)
+			return t;
 
-		if( compareResult < 0 )
-			t.left = remove( x, t.left );
-		else if( compareResult > 0 )
-			t.right = remove( x, t.right );
-		else if( t.left != null && t.right != null ){ // Two children
-			t.element = findMin( t.right ).element;
-			t.right = remove( t.element, t.right );
-		}
-		else
-			t = ( t.left != null ) ? t.left : t.right;
+		int compareResult = x.compareTo(t.getElement());
+
+		if (compareResult < 0)
+			t.left = remove(x, t.left);
+		else if (compareResult > 0)
+			t.right = remove(x, t.right);
+		else if (t.left != null && t.right != null) {
+			t.element = findMin(t.right).element;
+			t.right = remove(t.element, t.right);
+		} else
+			t = (t.left != null) ? t.left : t.right;
+
 		return t;
 	}
 
-	public void printRange(AnyType k1, AnyType k2, BinaryNode<AnyType> t){
+	public void printRange(AnyType k1, AnyType k2, BinaryNode<AnyType> t) {
 
-		if(t == null)
-			return ;
+		if (t == null)
+			return;
 
 		if (t.getElement().compareTo(k1) > 0 && t.getElement().compareTo(k2) < 0) {
 			System.out.println(t.getElement());
-			printRange(k1,k2,t.getRight());
-			printRange(k1,k2,t.getLeft());
+			printRange(k1, k2, t.getRight());
+			printRange(k1, k2, t.getLeft());
 		}
 		if (t.getElement().compareTo(k1) < 0)
-			printRange(k1,k2,t.getRight());
+			printRange(k1, k2, t.getRight());
 		else
-			printRange(k1,k2,t.getLeft());
-
+			printRange(k1, k2, t.getLeft());
 	}
 
-	public boolean isSimilar(BinaryNode t1, BinaryNode t2){
+	public boolean isSimilar(BinaryNode t1, BinaryNode t2) {
 
 		if (t1 == null && t2 == null)
 			return true;
@@ -127,89 +128,110 @@ public class BinarySearchTree<AnyType extends Comparable<AnyType>> {
 		else if (t1 != null && t2 != null) {
 			isSimilar(t1.right, t2.right);
 			return isSimilar(t1.left, t2.left);
-		}
-		else
+		} else
 			return false;
 	}
 
-	public int countNodes(BinaryNode<AnyType> t){
-		return countNodes(t,1);
-	}
 
-	private int countNodes(BinaryNode<AnyType> t, int count){
+	private int countNodes(BinaryNode<AnyType> t) {
+
 		if (t.right == null && t.left == null) { // is leaf
-			System.out.println(t.element);
+			count++;
 			return count;
 		}
-		 if (t.right !=null && t.left == null) { // only 1 child
-			System.out.println(t.element);
-			 return countNodes(t.right, count++);
-		 }
-		 if (t.right == null && t.left != null) {
-			 System.out.println(t.element);
-			 return countNodes(t.left, count++);
-		 }
-		else{
-			System.out.println(t.element);
-			countNodes(t.right,count++);
-			return countNodes(t.left,count++);
+		else if (t.right !=null && t.left == null) { // only right child
+			count++;
+			return countNodes(t.right);
+		}
+		else if (t.left != null && t.right == null) { // only left child
+			count++;
+			return countNodes(t.left);
+		}
+		else {  // 2 nodes
+			count++;
+			countNodes(t.right);
+			return countNodes(t.left);
 		}
 	}
 
-	public int countLeaves(BinaryNode<AnyType> t, int count){
-		if (t.isLeaf()) { // is leaf
-			return count++;
+	public int countLeaf(BinaryNode<AnyType> t) {
+
+		if (t.isLeaf()) {  // is leaf
+			leafCount++;
+			return leafCount;
 		}
-		if (t.right !=null && t.left == null) { // only 1 child
-			return countNodes(t.right, count++);
-		}
-		if (t.right == null && t.left != null) {
-			return countNodes(t.left, count++);
-		}
-		else{
-			System.out.println(t.element);
-			return countNodes(t.left,count++);
+		else if (t.right !=null && t.left == null)  // only right child
+			return countLeaf(t.right);
+
+		else if (t.left != null && t.right == null)  // only left child
+			return countLeaf(t.left);
+
+		else {  // 2 nodes
+			countLeaf(t.right);
+			return countLeaf(t.left);
 		}
 	}
 
-	private boolean isLeaf(BinaryNode<AnyType> t){
-		if (t.right == null && t.left == null)
-			return true;
-		else
-			return false;
+	public int countFullNodes(BinaryNode<AnyType> t){
+		if (t.right != null && t.left != null) {  // is full
+			fullNodeCount++;
+			countFullNodes(t.left);
+			return countFullNodes(t.right);
+		}
+		else if (t.right !=null && t.left == null)  // only right child
+			return countFullNodes(t.right);
+
+		else if (t.left != null && t.right == null)  // only left child
+			return countFullNodes(t.left);
+
+		else {  // leaf
+			return fullNodeCount;
+		}
 	}
 
-//	public int sumOfNodes(BinaryNode<AnyType> t){
-//		if (t.right == null && t.left == null) { // is leaf
-//			return count;
-//		}
-//		if (t.right !=null && t.left == null) { // only 1 child
-//			return countNodes(t.right, count++);
-//		}
-//		if (t.right == null && t.left != null) {
-//			return countNodes(t.left, count++);
-//		}
-//		else{
-//			countNodes(t.right,count++);
-//			return countNodes(t.left,count++);
-//		}
-//	}
+	public boolean searchTreeOrderProperty(BinaryNode<AnyType> t) {
+		if (t.left != null && t.right != null) {
+			if (t.left.element.compareTo(t.element) == 1 || t.right.element.compareTo(t.element) == -1)
+				return false;
+			else if (t.left.element.compareTo(t.element) == -1 || t.right.element.compareTo(t.element) == 1) {
+				searchTreeOrderProperty(t.left);
+				return searchTreeOrderProperty(t.right);
+			}
+		}
+		return true;
+	}
 
+	public static void main (String[] args) {
 
-	public static void main(String[] args){
-		BinarySearchTree t1 = new BinarySearchTree();
-
+		BinarySearchTree<Integer> bst = new BinarySearchTree<>();
 		root = new BinaryNode(21);
 
-		//create tree 1
-		for(int i=0;i<10;i++){
+		bst.insert(30, root);
+		bst.insert(10, root);
+		bst.insert(14, root);
+		bst.insert(16, root);
+		bst.insert(15, root);
+		bst.insert(8, root);
+		bst.insert(4, root);
+		bst.insert(2, root);
+		bst.insert(6, root);
+		bst.insert(46, root);
+		bst.insert(39, root);
+		bst.insert(69, root);
+
+	/*	for(int i=0;i<10;i++){
 			int temp = (int) (100*Math.random());
-			t1.insert(temp, t1.getRoot());
+			bst.insert(temp, bst.getRoot());
 		}
-
-
-		System.out.print(root.toString());
+*/
+		System.out.println(root.toString());
 		System.out.println();
-		//System.out.println(t1.countLeaves(root));
+		System.out.println("Full Nodes: "+bst.countFullNodes(root));
+		System.out.println("Count: " + bst.countNodes(root));
+		System.out.println("Leaf count: "+bst.countLeaf(root));
+		System.out.println("Min: " + bst.findMin(root));
+		System.out.println("Max: " + bst.findMax(root));
+		System.out.println("STOP: "+bst.searchTreeOrderProperty(root));
+
 	}
 }
--- src/Classwork/TestMap.java
+++ src/Classwork/TestMap.java
@@ -4,37 +4,66 @@ package Classwork;
 import java.util.*;
 public class TestMap {
 
-    private Map<String, String> map;
+	private Map<String, String> map;
 
-    public TestMap(){
-        map = new TreeMap<>();
-    }
+	public TestMap(){
+		map = new TreeMap<>();
+	}
 
-    public void insert(String x, String y){
-        if (!map.containsKey(x))
-            map.put(x,y);
+	public void insert(String x, String y){
+		if (!map.containsKey(x))
+			map.put(x,y);
 
-        Set<String> keys = map.keySet();
-    }
+		Set<String> keys = map.keySet();
+	}
+
+	public int size(){
+		return map.size();
+	}
+
+	public  void printMap(){
+		for (String key : map.keySet()){
+			System.out.println(key + " --> " + map.get(key));
+		}
+	}
+
+	private Set<String> keySet() {
+		return map.keySet();
+	}
+
+	private Object get(Object key){
+		return map.get(key);
+	}
+
+	private void remove(Object key){
+		map.remove(key);
+	}
+
+	public  void swap(){
+		Object[] tempKey = map.keySet().toArray();
+
+		for(int i=0;i<map.size();i++){
+			String tempValue =  map.get(tempKey[i]);
+		//	System.out.println(tempKey[i]);
+			map.put(tempValue,""+tempKey[i]);
+			map.remove(tempKey[i]);
+		}
+	}
+
+	public static void main(String[] args){
+		TestMap map = new TestMap();
+
+		map.insert("Evan", "A");
+		map.insert("Angel", "D");
+		map.insert("Alex", "F");
+		map.insert("Johnny", "B");
+		map.insert("Sullivan", "C");
 
 
-    public  void printMap(){
-        System.out.println("Using for each loop");
-        for (int i =0;i<map.size();i++) {
-            var key =
-             var value = map.values();
-                 System.out.println(key+" --> "+value);
-        }
-    }
-
-    public static void main(String[] args){
-        TestMap map = new TestMap();
-
-        map.insert("Evan", "A");
-        map.insert("Angel", "A");
-        map.insert("Alex", "F");
-        map.insert("Johnny", "B");
-
-        map.printMap();
-    }
+		System.out.println(map.size());
+		map.printMap();
+		System.out.println();
+		map.swap();
+		map.printMap();
+	}
 }
